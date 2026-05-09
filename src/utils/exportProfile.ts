eaf3a1f3d083280ca5125a38ab5333f5e3decddf
tkm <thekinkymap@gmail.com>
2026-05-09 17:36:47 +0700

    Update exportProfile.ts

--- src/utils/exportProfile.ts
+++ src/utils/exportProfile.ts
@@ -1,1 +1,22 @@
+// src/utils/exportProfile.ts
+import { IdentityState } from '../IdentityData';
+import { Preferences, PreferenceCategory } from '../data/preferences';
 
+interface ExportProps {
+  myName: string;
+  partnerName: string;
+  myRole: string;
+  partnerRole: string;
+  meIdentity: IdentityState;
+  partnerIdentity: IdentityState;
+  myPreferences: Preferences;
+  partnerPreferences: Preferences;
+  visibleCategories: PreferenceCategory[];
+  bothHavePreferences: boolean;
+  getShareableUrl: () => Promise<string>;
+}
+
+export const generateProfileText = async (props: ExportProps): Promise<string> => {
+  // Move all your text += logic here...
+  // This makes Index.tsx incredibly clean!
+}
